52b2ddaf06f0b8ede64bbef18d5776fbd5a38672
justzxid <zaidmk96@gmail.com>
2025-02-02 16:57:45 +0300

    Adding a delete functionality

--- server.js
+++ server.js
@@ -1,44 +1,52 @@
-
-require('dotenv').config();
-require('./config/database');
+// Dependencies
+const dotenv = require("dotenv")
+dotenv.config();
 const express = require('express');
-const morgan = require('morgan')
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require('morgan');
 
 const app = express();
 
-// MODELS
+// Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
 
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Model import
 const Fruit = require('./models/fruit');
 
-// MIDDLEWARE
+// Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
-app.use(morgan('dev'))
-app.use(express.urlencoded({extended: false}))
 
-// PUBLIC ROUTES
+// Public routes
 
+// index page
 app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
 
-app.get("/fruits", async (req, res) => {
-const allFruits = await Fruit.find();
-console.log(allFruits); // log the fruits!
-res.send("Welcome to the index page!");
-});
 
+// fruits page
 app.get("/fruits", async (req, res) => {
-    const allFruits = await Fruit.find();
-    res.render("fruits/index.ejs", { fruits: allFruits });
-  });
+const allFruits = await Fruit.find({});
+// console.log(allFruits); // log the fruits!
+res.render("fruits/index.ejs", { fruits: allFruits });
+});
 
 
 // Create Fruit
-
 app.get('/fruits/new', (req, res) => {
     res.render('fruits/new.ejs');
 })
 
+// Post Fruit
 app.post("/fruits", async (req, res) => {
     if (req.body.isReadyToEat === "on") {
       req.body.isReadyToEat = true;
@@ -46,14 +54,25 @@ app.post("/fruits", async (req, res) => {
       req.body.isReadyToEat = false;
     }
     await Fruit.create(req.body);
-    res.redirect("/fruits/new");
+    res.redirect("/fruits");
   });
 
-
-// LISTENER
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  // Show Fruit
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId)
+  res.render('fruits/show.ejs', {fruit : foundFruit});
 });
 
 
+// Delete Fruit
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+
+
+// listener
+app.listen(3100, () => {
+  console.log("Listening on port 3000");
+});
